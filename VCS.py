e0da6e5918766ea3a4a7566d34b77872cf4efa12
Billy <675957458@qq.com>
2018-08-23 14:23:29 +0800

    Set default Value of Value

--- VCS.py
+++ VCS.py
@@ -32,13 +32,13 @@ class VcsPanel(nukescripts.PythonPanel):
         self.proj_dir_dict = getData.getProjDirDict(self.jsData)
 
         # creat vertion re match
-        self.ver_check = re.compile(r'^\d\d\d_?\d{,3}$', re.IGNORECASE)
+        self.ver_check = re.compile(r'^\d\d\d_?\d{,3}.nk$', re.IGNORECASE)
 
         # use to match main version
-        self.ver_main_check = re.compile(r'^\d\d\d$', re.IGNORECASE)
+        self.ver_main_check = re.compile(r'^\d\d\d.nk$', re.IGNORECASE)
 
         # use to match sub veision
-        self.ver_sub_check = re.compile(r'^\d\d\d_\d{,3}$', re.IGNORECASE)
+        self.ver_sub_check = re.compile(r'^\d\d\d_\d{,3}.nk$', re.IGNORECASE)
 
         # get current file path
         self.current_file_path = scriptCheck.get_current_script_name()
@@ -46,14 +46,15 @@ class VcsPanel(nukescripts.PythonPanel):
         # check current file if it in tasks
         self.in_task = self.have_this_task(self.current_file_path, self.proj_dir_dict, self.ver_check)
 
+        # task info from current script file
+        self.cur_task_info = []
+
         # if current file is not untitle save it for safe~
         if self.in_task != 'untitle':
             nuke.scriptSave()
 
-        nuke.message(self.in_task)
-
         #CREATE KNOBS
-        self.projKnob = nuke.Enumeration_Knob('project','Proj',getData.get_pro_list(self.jsData))
+        self.projKnob = nuke.Enumeration_Knob('project','Proj',[i[5:] for i in getData.get_pro_list(self.jsData)])
         self.epsKnob = nuke.Enumeration_Knob('eps','',[])
         self.shotKnob = nuke.Enumeration_Knob('shot','',[])
         self.epsKnob.clearFlag(nuke.STARTLINE)
@@ -65,11 +66,11 @@ class VcsPanel(nukescripts.PythonPanel):
     @staticmethod
     def have_this_task(file_path, dir_dict, ver_check):
         '''
-        This function is make sure of have this
+        This function is make sure of have this task
         :param file_path:current nukeScript file path
         :param dir_dict:Project dir dict from json file
         :param ver_check:version check re funcion
-        :return: Bool if current file is in or not in tasks
+        :return: str if current file is in or not in tasks
         if current file is untitled return 'untitle'
         '''
 
@@ -97,19 +98,38 @@ class VcsPanel(nukescripts.PythonPanel):
         :param filepath: base name of current nuke file
         :return: list of task information sorted with: [proj,eps,shot]
         '''
+
         return filepath.split('_')[:2]
 
     def knobChanged(self, knob):
         if knob is self.projKnob or knob.name() == 'showPanel':
-            self.epsKnob.setValues(getData.get_eps_list(self.jsData,self.projKnob.value()))
-            self.shotKnob.setValues(getData.get_shot_list(self.jsData, self.projKnob.value(), self.epsKnob.value()))
+            self.refresh_panel_with_project('proj_'+self.projKnob.value())
+
         if knob is self.epsKnob:
-            self.shotKnob.setValues(getData.get_shot_list(self.jsData,self.projKnob.value(),self.epsKnob.value()))
-        if self.in_task == 'have':
+            self.shotKnob.setValues(getData.get_shot_list(self.jsData,'proj_'+self.projKnob.value(),self.epsKnob.value()))
+
+        if knob.name() == 'showPanel' and self.in_task == 'have':
+            '''
+            Set default value with current task if this script in task
+            '''
+            # get task info from fileName
             self.cur_task_info = self.get_task_info_from_file(os.path.basename(self.current_file_path))
-            self.projKnob.setValue('proj_'+self.cur_task_info[0])
+            # set Project Value
+            self.projKnob.setValue(self.cur_task_info[0])
+            # refresh Panel
+            self.refresh_panel_with_project('proj_'+self.projKnob.value())
+            # set eps and shot Panel
             self.epsKnob.setValue(self.cur_task_info[1])
             self.shotKnob.setValue(self.cur_task_info[2])
+
+    def refresh_panel_with_project(self,proj):
+        '''
+        This function is use to refresh when project panel refresh
+        :param proj:Value of project panel
+        :return:None
+        '''
+        self.epsKnob.setValues(getData.get_eps_list(self.jsData, proj))
+        self.shotKnob.setValues(getData.get_shot_list(self.jsData,proj, self.epsKnob.value()))
 
 
 # test:
